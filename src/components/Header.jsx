1318c37bcfa521c1528fa8441233004c7eaf160a
Christoffer Nielsen <cph-cn332@cphbusiness.dk>
2022-12-06 13:44:29 +0100

    profile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ import "../styles/header.css";
 import SignUpBtn from "./SignUpBtn.jsx";
 import SignInBtn from "./SignInBtn.jsx";
 import logo from "../images/logo.png";
+import userFacade from "../utils/userFacade.js";
 
 
 function Header({loggedIn, setLoggedIn, userFacade}) {
@@ -13,6 +14,7 @@ function Header({loggedIn, setLoggedIn, userFacade}) {
         <nav className="topnav">
             <NavLink className="nav-logo" to="/"><i className="fa fa-heartbeat"></i></NavLink>
             <NavLink className="nav-home" to="/"><i></i> Home</NavLink>
+            {loggedIn ? (<NavLink className="profileIcon" to="profile"><i className="fa fa-user"></i> {userFacade.getUserName()}</NavLink>) : <></>}
 
             {!loggedIn ? (<SignUpBtn/>) : (<div></div>)}
 
